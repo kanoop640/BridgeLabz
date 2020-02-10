f7736ba9b358716df9f9a176d2827c86c9106408
kanoop640 <kanoop640@gmail.com>
2020-02-10 17:11:52 +0530

    [Anoop]function

--- LoggerTest/Program.cs
+++ LoggerTest/Program.cs
@@ -19,8 +19,14 @@ namespace LoggerTest
             Console.WriteLine("hello .net ");
             log.Info("Helo This is info statement");
             log.Error("It is system error");
-           
             Console.ReadLine();
+            add(10, 20);
+        }
+
+        public static void add(int x, int y)
+        {
+            int z = x + y;
+            Console.WriteLine(z);
         }
     }
 }
